42ff699d9e2ee796d34140c7e03f3822f3f89eae
Lawrence Kim <lawrence.tw.kim@gmail.com>
2020-03-16 02:44:04 +0900

    delete unnecessary buttons

--- project-data.py
+++ project-data.py
@@ -52,24 +52,19 @@ class visualialog(QDialog) :
         self.label1 = QLabel("")
         self.label2 = QLabel("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-        self.pushButton1 = QPushButton("ALL PIZZA BRAND")
+        self.pushButton1 = QPushButton("Pizza Brand Count")
         self.pushButton1.clicked.connect(self.pushButton1Clicked)
-        self.pushButton2 = QPushButton("PIZZA BRAND IN BUSAN")
+        self.pushButton2 = QPushButton("Average Housing Price")
         self.pushButton2.clicked.connect(self.pushButton2Clicked)
-        self.pushButton3 = QPushButton("SALESPRICE PER 3.3㎥")
+        self.pushButton3 = QPushButton("Korea Commercial Area")
         self.pushButton3.clicked.connect(self.pushButton3Clicked)
-        self.pushButton4 = QPushButton("KOREA COMMERCIAL")
+        self.pushButton4 = QPushButton("Pizza Brand Startup Cost")
         self.pushButton4.clicked.connect(self.pushButton4Clicked)
-        self.pushButton5 = QPushButton("KOREA SINGLES RATIO")
-        self.pushButton5.clicked.connect(self.pushButton5Clicked)
-        self.pushButton6 = QPushButton("MIN FOUNDING COST")
-        self.pushButton6.clicked.connect(self.pushButton6Clicked)
         
         # Table Widget
         global tableWidget
         self.tableWidget = QTableWidget(self)
         self.tableWidget.resize(500, 500)
-        #self.setTableWidgetData()
 
         # Left Layout
         global leftLayout
@@ -82,8 +77,6 @@ class visualialog(QDialog) :
         rightLayout.addWidget(self.pushButton2)
         rightLayout.addWidget(self.pushButton3)
         rightLayout.addWidget(self.pushButton4)
-        rightLayout.addWidget(self.pushButton5)
-        rightLayout.addWidget(self.pushButton6)
         rightLayout.addWidget(self.label2)
         rightLayout.addWidget(self.label1)
         rightLayout.addStretch(1)   
@@ -96,7 +89,7 @@ class visualialog(QDialog) :
 
         self.setLayout(layout)
 
-
+    # Pizza Brand Count
     def pushButton1Clicked(self) :
         pizza = pd.read_csv("./Data/pizza_all_count.csv")
         pizza.fillna(0, inplace = True)
@@ -130,40 +123,8 @@ class visualialog(QDialog) :
             self.tableWidget.setItem(idx, 5, QTableWidgetItem(total))
             self.tableWidget.setItem(idx, 6, QTableWidgetItem(mr_pizza))
     
+    # Average Housing Price
     def pushButton2Clicked(self) :
-        busan_pizza = pd.read_csv("./Data/부산피자.csv")
-        busan_pizza.fillna(0, inplace = True)
-
-        self.tableWidget.setRowCount(busan_pizza.shape[0])             # table을 2 x 2로 만듦
-        self.tableWidget.setColumnCount(busan_pizza.shape[1])
-        self.label1.setText("The total number of data instances are: " + str(busan_pizza.shape[0]))
-        self.tableWidget.setHorizontalHeaderLabels(list(busan_pizza.columns))
-
-        pizzaData = {}
-        pizzaLists = []
-        for i in range(len(busan_pizza)) :
-            for row in busan_pizza.itertuples():
-                pizzaData['city'] = row.시도
-                pizzaData['district'] = row.시군구
-                pizzaData['store'] = row.지점명
-                pizzaData['address'] = row.주소
-                pizzaData['lng'] = str(round(row.lng,3))
-                pizzaData['lat'] = str(round(row.lat,3))
-                pizzaData['brand'] = row.브랜드
-                pizzaLists.append(list(pizzaData.values()))
-
-        data = pizzaLists
-
-        for idx, (city, district, store, address, lng, lat, brand) in enumerate(pizzaLists): 
-            self.tableWidget.setItem(idx, 0, QTableWidgetItem(city))
-            self.tableWidget.setItem(idx, 1, QTableWidgetItem(district))
-            self.tableWidget.setItem(idx, 2, QTableWidgetItem(store))
-            self.tableWidget.setItem(idx, 3, QTableWidgetItem(address))
-            self.tableWidget.setItem(idx, 4, QTableWidgetItem(lng))
-            self.tableWidget.setItem(idx, 5, QTableWidgetItem(lat))
-            self.tableWidget.setItem(idx, 6, QTableWidgetItem(brand))
-    
-    def pushButton3Clicked(self) :
         for_sales = pd.read_csv("./Data/지역별 평당 매매가.csv")
         for_sales.rename(columns = {'평당 매매가' : '평당매매가'}, inplace = True)
         for_sales.fillna(0, inplace = True)
@@ -191,7 +152,8 @@ class visualialog(QDialog) :
             self.tableWidget.setItem(idx, 2, QTableWidgetItem(population))
             self.tableWidget.setItem(idx, 3, QTableWidgetItem(salesPrice))
 
-    def pushButton4Clicked(self) :
+    # Korea Commercial Area
+    def pushButton3Clicked(self) :
         commercial = pd.read_csv("./Data/전국 상권 최종본.csv")
 
         self.label1.setText("The total number of data instances are: " + str(commercial.shape[0]) )
@@ -221,33 +183,8 @@ class visualialog(QDialog) :
             self.tableWidget.setItem(idx, 2, QTableWidgetItem(lng))
             self.tableWidget.setItem(idx, 3, QTableWidgetItem(lat))
 
-    def pushButton5Clicked(self) :
-        singles = pd.read_csv("./Data/1인가구.csv")
-        singles.rename(columns = {'시 도' : '시도', '시 군 구' :'시군구', '1인 가구 비율' : '일인가구비율'}, inplace = True)
-        self.label1.setText("The total number of data instances are: " + str(singles.shape[0]) )
-
-        self.tableWidget.setRowCount(singles.shape[0])             # table 2 x 2
-        self.tableWidget.setColumnCount(singles.shape[1])
-
-        self.tableWidget.setHorizontalHeaderLabels(list(singles.columns))
-
-        pizzaData = {}
-        pizzaLists = []
-        for i in range(len(singles)) :
-            for row in singles.itertuples() :
-                pizzaData['city'] = row.시도
-                pizzaData['district'] = row.시군구
-                pizzaData['SinglesRatio'] = str(row.일인가구비율)
-                pizzaLists.append(list(pizzaData.values()))
-
-        data = pizzaLists
-
-        for idx, (city, district, SinglesRatio) in enumerate(pizzaLists): 
-            self.tableWidget.setItem(idx, 0, QTableWidgetItem(city))
-            self.tableWidget.setItem(idx, 1, QTableWidgetItem(district))
-            self.tableWidget.setItem(idx, 2, QTableWidgetItem(SinglesRatio))
-    
-    def pushButton6Clicked(self) :
+    # Pizza Brand Startup Cost  
+    def pushButton4Clicked(self) :
         cost = pd.read_csv("./Data/피자 브랜드별 최소 창업비용.csv")
         cost.rename(columns = {'평당 매매가' : '평당매매가'}, inplace = True)
         self.label1.setText("The total number of data instances are: " + str(cost.shape[0]) )
